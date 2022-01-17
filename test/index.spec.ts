c7396dffe0b5d8b9844b76c7e9e67bfae39b80ea
Glen Maddern <glenmaddern@gmail.com>
2022-01-17 13:50:44 +0000

    fixed the tests, we can iterate

--- test/index.spec.ts
+++ test/index.spec.ts
@@ -1,5 +1,5 @@
 import worker from '@/index'
-import {ctx} from './utils'
+import { ctx, testGraphql } from './utils'
 // @ts-ignore
 import mapValuesDeep from 'map-values-deep'
 
@@ -12,7 +12,7 @@ test('should redirect to example page on no route match', async () => {
 describe('single object', () => {
   beforeEach(async () => {
     const env = getMiniflareBindings()
-    const {HOLODB_USER} = env
+    const { HOLODB_USER } = env
     const id = HOLODB_USER.idFromName('glen')
     const storage = await getMiniflareDurableObjectStorage(id)
     await storage.put('email', 'glen@glen.com')
@@ -20,33 +20,30 @@ describe('single object', () => {
   })
 
   test('single field', async () => {
-    const env = getMiniflareBindings()
-    const req = new Request('http://localhost/graphql', {
-      method: 'POST',
-      body: `
+    await testGraphql(
+      `
         query {
           getUserByUsername(username: "glen") {
             email
           }
         }
       `,
-    })
-    const res = await worker.fetch(req, env, ctx)
-    expect([res.status, await res.json()]).toMatchObject([
       200,
       {
         ok: true,
         errors: [],
-        data: {getUserByUsername: {email: 'glen@glen.com'}},
-      },
-    ])
+        data: {
+          getUserByUsername: {
+            email: 'glen@glen.com',
+          },
+        },
+      }
+    )
   })
 
   test('two fields', async () => {
-    const env = getMiniflareBindings()
-    const req = new Request('http://localhost/graphql', {
-      method: 'POST',
-      body: `
+    await testGraphql(
+      `
         query {
           getUserByUsername(username: "glen") {
             email
@@ -54,21 +51,24 @@ describe('single object', () => {
           }
         }
       `,
-    })
-    const res = await worker.fetch(req, env, ctx)
-    expect([res.status, mapValuesDeep(await res.json(), (x: any) => x === null ? '<null>' : x)]).toMatchObject([200,
+      200,
       {
         ok: true,
         errors: [],
-        data: {getUserByUsername: {email: 'glen@glen.com', avatar: 'https://www.fillmurray.com/250/250'}},
-      }]
+        data: {
+          getUserByUsername: {
+            email: 'glen@glen.com',
+            avatar: 'https://www.fillmurray.com/250/250',
+          },
+        },
+      }
     )
   })
 })
 
 test.skip('should pass-through to durable object', async () => {
   const env = getMiniflareBindings()
-  const {COUNTER, HOLODB_USER} = env
+  const { HOLODB_USER } = env
   const id = HOLODB_USER.idFromName('glen')
   const storage = await getMiniflareDurableObjectStorage(id)
   await storage.put('email', 'glen@glen.com')
@@ -92,7 +92,7 @@ test.skip('should pass-through to durable object', async () => {
   expect({
     status: res.status,
     body: await res.json(),
-  }).toMatchObject({ok: true})
+  }).toMatchObject({ ok: true })
 
   const newValue = await storage.get('count')
   expect(newValue).toBe(11)
--- test/utils.ts
+++ test/utils.ts
@@ -1,5 +1,23 @@
+import worker from "@/index";
+// @ts-ignore
+import mapValuesDeep from 'map-values-deep'
+
+export async function testGraphql(query: string, status: number, expected: any) {
+  const env = getMiniflareBindings()
+  const req = new Request('http://localhost/graphql', {
+    method: 'POST',
+    body: query
+  })
+  const res = await worker.fetch(req, env, ctx)
+  expect([
+    res.status,
+    mapValuesDeep(await res.json(), (x: any) => x === null ? '<null>' : x)
+  ]).toMatchObject([status, expected])
+}
+
 export const ctx: ExecutionContext = {
   waitUntil(promise: Promise<any>) {
   },
-  passThroughOnException() {}
+  passThroughOnException() {
+  }
 }
